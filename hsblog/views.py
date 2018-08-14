7ae085376e40dabbae5f4c7658e7a6c862d352b8
hslee9510 <hslee9510@kaist.ac.kr>
2018-08-14 17:39:59 +0900

    start real game

--- hsblog/views.py
+++ hsblog/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from .models import Posths,Post
+
 
 def index(request):
-    return render(request, 'blog/index.html')
+    return render(request, 'blog/daily_hs.html')
